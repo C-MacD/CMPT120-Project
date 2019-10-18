b92da7d8514b20586c61ff61a7eba69c01bb0fb8
“C-MacD” <colin.macdonald1@marist.edu>
2019-10-18 17:09:26 -0400

    Made pep8 compliant.  Resolves #11

--- game.py
+++ game.py
@@ -5,22 +5,32 @@
 def main():
     # Initialize some variables.
     score = 0
-    #locations = ["bedroom", "hallway", "stairway", "street"]
+    # locations = ["bedroom", "hallway", "stairway", "street"]
     currentLocation = 0
 
-    bedroom = "You are standing in a creepy old smelly " + \
-        "bedroom filled with cockroaches crawling all over the bedsheets. You start to hear footsteps coming towards you."
+    bedroom = ("You are standing in a creepy old smelly \n"
+               "bedroom filled with cockroaches crawling all over the \n"
+               "bedsheets. You start to hear footsteps coming towards you.")
 
-    hallway = "You run into a hallway. The walls are filled claw marks all over them. Water is dripping from the ceiling" +\
-        ". Someone whispers to you and tells you to get out."
+    hallway = ("You run into a hallway. The walls are filled claw marks \n"
+               "all over them. Water is dripping from the ceiling. \n"
+               "Someone whispers to you and tells you to get out.")
 
-    stairway = "You end up in a dark noisy staircase. Rats squeak and voices scream at you, making you scared and terrified. You tell yourself to find an exit but you are lost and don't know " + \
-        "which stairway can lead to the exit. You see a little girl run down" + \
-        " the stairway on your left and you decide to follow her."
+    stairway = ("You end up in a dark noisy staircase. Rats squeak and \n"
+                "voices scream at you, making you scared and terrified. \n"
+                "You tell yourself to find an exit but you are lost and \n"
+                "don't know which stairway can lead to the exit. \n"
+                "You see a little girl run down the stairway on your left \n"
+                "and you decide to follow her.")
 
-    street = "You are standing in a street with nobody walking or no cars passing by. Only one light is on and it is flickering. The little girl you followed has led you outside. The little girl is waiting for you on the other side of the " + \
-        "street. As you get closer, you reach out to her and check if she is okay. The little girl slowly opens her mouth and shows " + \
-        "her sharp teeth and rips your arm off. You are never seen again."
+    street = ("You are standing in a street with nobody walking or no \n"
+              "cars passing by. Only one light is on and it is flickering. \n"
+              "The little girl you followed has led you outside. \n"
+              "The little girl is waiting for you on the other side of the \n"
+              "street. As you get closer, you reach out to her and check \n"
+              "if she is okay. The little girl slowly opens her mouth and \n"
+              "shows her sharp teeth and rips your arm off. \n"
+              "You are never seen again.")
 
     def scorePrint():
         """This function prints the score.
@@ -36,12 +46,12 @@ def main():
     # Welcome message
     print("██▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄██ ")
     print("Welcome to the Dark Zone")
-    print("""Life is normal.  Or so you think.  
-One day you are strolling along outside, enjoying the nice weather.  
-Then your vision fades to black and you fall to the ground and pass out.  
-When you wake up, you are somewhere else, somewhere you have never been before, with no memories of how you got there. 
-You are in a dimly lit room.
-You are terrified.""")
+    print("Life is normal.  Or so you think.  \n"
+          "One day you are strolling along outside, enjoying the nice \n"
+          "weather.  Then your vision fades to black and you fall to the \n"
+          "ground and pass out.  When you wake up, you are somewhere else, \n"
+          "somewhere you have never been before, with no memories of how \n"
+          "you got there. You are in a dimly lit room. You are terrified.")
 
     print()
     print()
